a4cac08c1849a13681ec77a7d0c587a17686616e
jairorossi <jairo.rossi@gmail.com>
2022-01-28 16:22:09 -0300

    surya: Snap -> GoogleCamera
    Swap AOSP camera for GoogleCamera

--- device.mk
+++ device.mk
@@ -113,7 +113,7 @@ PRODUCT_PACKAGES += \
     libshim_megvii
 
 PRODUCT_PACKAGES += \
-    Snap
+    GoogleCamera
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
